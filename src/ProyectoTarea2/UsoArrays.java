40c6889d33d9c5dce771ea4947d0efd515260b39
Hamza <hderouiche01@educarex.es>
2023-12-05 13:43:24 +0100

    Modficaciones

--- src/ProyectoTarea2/UsoArrays.java
+++ src/ProyectoTarea2/UsoArrays.java
@@ -1,12 +1,36 @@
 package ProyectoTarea2;
+
+import javax.swing.JOptionPane;
+
+import ProyectoTarea2.VectoresNombres.VectorNombres;
+
 public class UsoArrays {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 
-		Arrays miArray = new Arrays(5);
+		int opcion = Integer.parseInt(JOptionPane.showInputDialog("Dame una opción (1-2)"));
 
-		miArray.getDatosArray();
+		if (opcion == 1) {
+
+			Arrays miArray = new Arrays(5);
+
+			miArray.getDatosArray();
+
+		} else {
+			if (opcion == 2) {
+
+				int cont = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos nombres quieres introducir?"));
+				VectorNombres vNombre = new VectorNombres();
+
+				for (int i = 0; i < cont; i++) {
+					String nom = JOptionPane.showInputDialog("Dame un nombre:");
+					vNombre.setGuardarNombre(nom);
+				}
+
+				vNombre.getNombres();
+			}
+		}
 
 	}
 }
